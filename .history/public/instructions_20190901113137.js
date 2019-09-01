9d5e7b4b94cdea50d5d543afb50f4b5e6a9faa23
beltranrengifo <beltran@rengifo.es>
2019-09-01 17:00:31 +0200

    RM histoy

--- .history/public/instructions_20190901113137.js
+++
@@ -1,7 +0,0 @@
-export default {
-  mail: [
-    'Completa la firma y copia el resultado usando el botón de copiar',
-    'Preferencias > Firmas > Añadir nueva firma',
-    'Desmarcar el check "Usar siempre mi tipo de letra predefinido". Pegar el contenido copiado en la caja para el contenido.'
-  ]
-}
